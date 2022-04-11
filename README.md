da34076014919d6e70231e615f9b86b7df97f5c8
Patrick Bourgeois <patrickbourgeois@live.com>
2022-04-11 13:37:30 -0300

    First

---
+++ README.md
@@ -0,0 +1,4 @@
+# Docs will be contained here :)
+
+
+
---
+++ src/code.js
@@ -0,0 +1,55 @@
+// Functions
+
+
+// docs here
+function onOpen() {
+	SpreadsheetApp.getUi().createAddonMenu()
+		.addItem('Open Sidebar', 'openSidebar')
+		.addItem('Hello World', 'helloWorld')
+		.addToUi();
+}
+
+
+// docs here
+function onInstall() {
+	onOpen();
+}
+
+
+
+
+function openSidebar() {
+
+	// Name of html file as parameter
+	const html = HtmlService.createTemplateFromFile('sidebar').evaluate();
+
+	const ui = SpreadsheetApp.getUi();
+
+	ui.showSidebar(html);
+
+}
+
+
+
+function helloWorld() {
+
+	SpreadsheetApp.getActiveSpreadsheet().toast('Hello World!', 'Important Message');
+
+}
+
+
+
+
+function CUSTOMFUNCTION(num) {
+	return num * 2;
+}
+
+
+
+
+
+
+
+
+
+
